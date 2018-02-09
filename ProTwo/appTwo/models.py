23103bc29e89ede83b6bc9e412c3d6307085a85a
patrrb3 <34636285+patrrb3@users.noreply.github.com>
2018-02-09 12:48:37 -0500

    userprofile model

--- ProTwo/appTwo/models.py
+++ ProTwo/appTwo/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfileInfo (models.Model):
-    user = models.ForeignKey(User, on_delete = models.PROTECT)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
     department = models.CharField(max_length=128)
 
     def __str__(self):
